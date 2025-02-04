402f591cf719a678961518d4314243750613201f
kolinzo1 <kolaoyedele@icloud.com>
2025-02-04 16:50:32 -0500

    Switch to bcryptjs

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const bcrypt = require("bcrypt");
+const bcryptjs = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 router.post("/login", async (req, res) => {
