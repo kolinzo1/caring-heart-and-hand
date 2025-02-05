10fa0fb6fe327c3158d7ea5857b38a190fd94212
kolinzo1 <kolaoyedele@icloud.com>
2025-02-04 21:39:14 -0500

    added trustProxy to rateLimit

--- server.js
+++ server.js
@@ -46,7 +46,9 @@ const pool = mysql.createPool({
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   max: 100, // limit each IP to 100 requests per windowMs
-  message: "Too many requests from this IP, please try again later.",
+  standardHeaders: true,
+  legacyHeaders: false,
+  trustProxy: true, // Add this for Heroku
 });
 
 app.get("/", (req, res) => {
