9e12357e292b530babd3aede8a77e1bb981bf219
kolinzo1 <kolaoyedele@icloud.com>
2025-02-07 16:19:35 -0500

    updated server.js

--- server.js
+++ server.js
@@ -125,8 +125,7 @@ app.use(helmet.xssFilter());
 // CORS configuration
 app.use(
   cors({
-    origin:
-      "https://caring-heart-and-hand-client-4fcobjyc-kolinzo1s-projects.vercel.app",
+    origin: "https://caring-heart-and-hand-client.vercel.app",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
