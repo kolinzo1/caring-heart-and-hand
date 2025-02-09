0a9a24a3dfb4447e2616969d5d688f42cb8e961c
kolinzo1 <kolaoyedele@icloud.com>
2025-02-09 14:18:55 -0500

    updated adminRoutes

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,9 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const {
-  authMiddleware,
-  adminMiddleware,
-} = require("../middleware/authMiddleware");
+const { authMiddleware } = require("../middleware/authMiddleware");
+const { adminMiddleware } = require("../middleware/adminMiddleware");
 
 // Apply auth middleware to all admin routes
 router.use(authMiddleware);
