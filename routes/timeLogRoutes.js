0efe1b861ea13dd217286fb7581f4d895c7037da
kolinzo1 <kolaoyedele@icloud.com>
2025-02-09 21:08:43 -0500

    updated timeLogRoutes

--- routes/timeLogRoutes.js
+++ routes/timeLogRoutes.js
@@ -1,9 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const {
-  authMiddleware,
-  adminMiddleware,
-} = require("../middleware/authMiddleware");
+const { authMiddleware } = require("../middleware/authMiddleware");
+const { adminMiddleware } = require("../middleware/adminMiddleware");
 
 // Admin Routes
 router.get("/admin", [authMiddleware, adminMiddleware], async (req, res) => {
