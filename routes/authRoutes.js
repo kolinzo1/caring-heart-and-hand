b040e87bfd9f6a21d6ace148ada06ca8c5036bf8
kolinzo1 <kolaoyedele@icloud.com>
2025-03-13 16:50:29 -0400

    revertwd authRoutes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -9,9 +9,8 @@ const loginValidation = [
   check("email", "Please include a valid email").isEmail(),
   check("password", "Password is required").exists(),
 ];
-const corsMiddleware = require("../middleware/corsMiddleware");
 
-router.post("/login", corsMiddleware, async (req, res) => {
+router.post("/login", async (req, res) => {
   try {
     const { email, password } = req.body;
 
